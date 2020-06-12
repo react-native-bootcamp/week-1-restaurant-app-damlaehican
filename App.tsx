deffa09ccb113a4a91669298c2eb9b2cf62e45f6
Damla <35731884+damlaehican@users.noreply.github.com>
2020-06-12 05:42:14 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Restaurant from './src/Restaurant'
+import Rest from './src/components/RestaurantCard'
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Restaurant></Restaurant>
     </View>
   );
 }
@@ -12,8 +16,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
